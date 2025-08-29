a37340de7c0a4c22f4e605d93a0432743a3b35ea
igorskyflyer <igor.dvlpr@gmail.com>
2025-08-29 17:18:32 +0200

    Update CI script

--- scripts/make-git.js
+++ scripts/make-git.js
@@ -1,56 +1,45 @@
-import { writeFileSync, mkdirSync } from "node:fs"
-import { join } from 'node:path'
+import { writeFileSync, mkdirSync } from 'node:fs'
+import { join, resolve } from 'node:path'
 
-const PATH_TEST='./test'
-const PATH_GIT='.git'
 const PATH_NO_URL='is-git-no-url'
 const PATH_URL='is-git-with-url'
-const PATH_CONFIG='config'
 
-const gitConfig=`
-[core]
-	repositoryformatversion = 0
-	filemode = false
-	bare = false
-	logallrefupdates = true
-	symlinks = false
-	ignorecase = true
+function setUpGit(name, withUrl=false) {
+  const workTree=resolve('test', name)
+  const gitDir=resolve('test', `${name}__git`)
+
+  mkdirSync(workTree, { recursive: true })
+  mkdirSync(join(gitDir, 'refs', 'heads'), { recursive: true })
+  mkdirSync(join(gitDir, 'objects'), { recursive: true })
+
+  writeFileSync(join(workTree, '.git'), `gitdir: ${gitDir}\n`)
+  writeFileSync(join(gitDir, 'HEAD'), 'ref: refs/heads/main\n')
+
+  const baseConfig=`[core]
+repositoryformatversion = 0
+filemode = false
+bare = false
+logallrefupdates = true
+symlinks = false
+ignorecase = true
 `
-
-const gitConfigUrl=`
-[core]
-	repositoryformatversion = 0
-	filemode = false
-	bare = false
-	logallrefupdates = true
-	symlinks = false
-	ignorecase = true
+  const config=withUrl
+    ? `${baseConfig}
 [remote "origin"]
-	url = https://github.com/igorskyflyer/npm-duoscribi
-	fetch = +refs/heads/*:refs/remotes/origin/*
-`
+url = https://github.com/igorskyflyer/npm-duoscribi
+fetch = +refs/heads/*:refs/remotes/origin/*
+` :baseConfig
 
-function setUpGit(path) {
-  const gitPath=join(PATH_TEST, path, PATH_GIT)
-  const configPath=join(gitPath, PATH_CONFIG)
-  const withUrl=path.includes('with')
-
-  mkdirSync(gitPath, { recursive: true })
-
-  writeFileSync(
-    configPath,
-    withUrl? gitConfigUrl:gitConfig,
-    'utf8'
-  )
-
+  writeFileSync(join(gitDir, 'config'), config, 'utf8')
 
   // biome-ignore lint/suspicious/noConsole: needed for CI
-  console.info(`Successfully created: ${configPath} 🚀`)
+  console.info(`Successfully created the mock folder! 🚀`)
 }
 
 try {
   setUpGit(PATH_NO_URL)
-  setUpGit(PATH_URL)
-
+  setUpGit(PATH_URL, true)
+} catch (e) {
+  // biome-ignore lint/suspicious/noConsole: needed for CI
+  console.error(e)
 }
-catch { }
